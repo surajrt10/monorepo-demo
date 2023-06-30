17a08c8f5f853498d7693c31bb7e1cbcc4a528a2
surajrt10 <93704780+surajrt10@users.noreply.github.com>
2023-06-30 16:52:17 +0530

    Delete packages/app directory

--- packages/app/core/src/CustomButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Button } from '@mui/material'
-import './CustomButton.css'
-export const CustomButton = (props) => {
-  return (
-    <Button className='customButton' {...props}>{props.children}</Button>
-  )
-}
-
--- packages/app/core/src/index.js
+++
@@ -1,8 +0,0 @@
-// Typography
-export { Title, SubTitle, Paragraph } from './Typography'
-
-// Custom Buttons
-export { CustomButton } from './CustomButton'
-
-// Cards for statistical graphs
-export { GraphCard } from './GraphCard'
